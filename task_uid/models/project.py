84fb1dd34248f55e60d980e03cb0879ceb1bb065
MrDan4es <mrdan4es@gmail.com>
2023-02-09 20:26:03 +0500

    feat: added squence & views

--- task_uid/models/project.py
+++ task_uid/models/project.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class Project(models.Model):
@@ -12,13 +12,21 @@ class Project(models.Model):
         ),
     ]
 
-    project_code = fields.Char('Project tasks UID prefix')
-    next_task_id = fields.Integer(
-        'Next id for task', compute='_compute_next_id'
-    )
+    project_code = fields.Char('Код задач')
+    project_task_sequence = fields.Many2one('ir.sequence')
 
-    def _compute_next_id(self):
-        self.next_task_id = 1
+    def create_sequence(self):
+        self.ensure_one()
+        code = f'project.project.project_code.{self.project_code}'
+        sequence = self.env['ir.sequence'].search([('code', '=', code)])
+        if not sequence:
+            name = f'project {self.name}'
+            sequence = self.env["ir.sequence"].sudo().create({
+                'code': code,
+                'name': name
+            })
+            self.sudo().project_task_sequence = sequence
+            return sequence
 
 
 class Task(models.Model):
@@ -32,10 +40,32 @@ class Task(models.Model):
         ),
     ]
 
-    task_uid = fields.Char('Task UID')
+    task_uid = fields.Char('Код задачи')
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        print(vals_list)
-        projects = super().create(vals_list)
-        return projects
+    @api.model_create_single
+    def create(self, vals):
+        project = self.env['project.project'].search(
+            [('id', '=', vals['project_id'])]
+        )
+        code = project.project_code
+        if code:
+            sequence = self.env['ir.sequence'].search([(
+                'code', '=', f'project.project.project_code.{code}'
+            )])
+            if not sequence:
+                sequence = project.create_sequence()
+            id = sequence.next_by_id()
+            vals['task_uid'] = f'{project.project_code}-{id:0>3}'
+
+        return super(Task, self).create(vals)
+
+    @api.depends('name', 'task_uid')
+    def name_get(self):
+        tasks = []
+
+        for task in self:
+            prefix = f'[{task.task_uid}] ' if task.task_uid else ''
+            name = prefix + task.name
+            tasks.append((task.id, name))
+
+        return tasks
